49244dd520a1359167fa7244d21453fcbb60ad0b
dreamlx <dreamlinx@gmail.com>
2013-10-13 15:28:30 +0800

    change credit start balance

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -3,7 +3,7 @@ class Credit < ActiveRecord::Base
   has_many :credit_line_items, order: 'created_at'
 
   #TODO: 默认值应该是0，这里以后需要手工调整
-  default_value_for :start_balance, 0
+  default_value_for :start_balance, 1000
   default_value_for :balance, 0
 
   def self.bundles
@@ -19,12 +19,11 @@ class Credit < ActiveRecord::Base
   end
 
   def add_expense(stage)
-    #stage.price
-    self.credit_line_items.create(amount: -(0), stage: stage)
+    self.credit_line_items.create(amount: -(stage.price), stage: stage)
   end
 
   def update_balance
-    self.balance = credit_line_items.map(&:amount).sum
+    self.balance = credit_line_items.map(&:amount).sum + self.start_balance
     self.save!
   end
 end
