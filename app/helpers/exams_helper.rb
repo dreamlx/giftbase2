e9985aa67a4f26220b642c3df4668b523c756159
小龙 <duxiaolong92@gmail.com>
2013-11-22 11:20:00 +0800

    modife take_exam

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -4,7 +4,7 @@ module ExamsHelper
   	answer = question_line_item.answers.find_by_exam_id(exam.id)
   	question = question_line_item.question
   	if answer.nil?
-     return t("show", scope: "crud")
+     return Answer.human_attribute_name("user_answer_nil")
     else
       question.single_choice_options.to_enum.with_index(65).each do |op, i|
         return i.chr if answer.option_id.to_i == op.id
