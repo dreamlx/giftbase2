a7bf37350f806efd137d6ae20cd1a7047c9f495b
HuberyDu <duxiaolong92@gmail.com>
2014-02-25 13:58:02 +0800

    modifed question_level update bug

--- app/controllers/admin/question_levels_controller.rb
+++ app/controllers/admin/question_levels_controller.rb
@@ -25,7 +25,7 @@ module Admin
     def update
       @question_level = QuestionLevel.find(params[:id])
 
-      if @question_level.update_attributes(params[:admin_question_level])
+      if @question_level.update_attributes(params[:question_level])
         redirect_to admin_question_levels_path, notice: t("success", scope: "flash.controller.update", model: QuestionLevel.model_name.human)
       else
         render action: "edit"
