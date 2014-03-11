9fbf23950e520b79a38502e15b4506a87e4953b9
dreamlx <dreamlinx@gmail.com>
2014-03-11 20:15:59 +0800

    generate class no

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
   has_many :grades, through: :user_grades
 
   has_one :credit
-
+  before_validation :generate_calss_no, on: :create
   before_save :ensure_authentication_token
   after_create :create_its_credit
 
@@ -78,6 +78,18 @@ class User < ActiveRecord::Base
 
   def create_its_credit
     self.create_credit
+  end
+
+  def generate_class_no
+    if self.class_no.blank?
+      record = true
+      while record
+        random = rand(1000000000).to_s.rjust(9, '0')
+        record = self.class.where(number: random).first
+      end
+      self.class_no = random
+    end
+    self.class_no
   end
 
 
