22dd66216bc37385a0c7574e223cfc569cdc4515
dreamlx <dreamlinx@gmail.com>
2014-03-17 21:33:02 +0800

    fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  validates_uniqueness_of :email, :username, :class_no, :qq, :user
+  validates_uniqueness_of :email, :username, :class_no, :qq
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
