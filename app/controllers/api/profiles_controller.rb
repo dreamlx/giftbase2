9615bc6669e1a599248a0188bf8abcc98416ce75
dreamlx <dreamlinx@gmail.com>
2014-04-30 16:56:58 +0800

    support cn

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 module Api
   class ProfilesController < Api::BaseController
     before_filter :authenticate_user!
@@ -8,12 +9,12 @@ module Api
     end
 
     def update
-      @user             = current_user
-      @user.avatar      = params[:avatar] unless params[:avatar].blank?
-      @user.avatar_id      = params[:avatar_id] unless params[:avatar_id].blank?
-      @user.qq          = params[:qq] unless params[:qq].blank?
-      @user.birthday    = params[:birthday] unless params[:birthday].blank?
-      @user.home_address         = params[:home_address] unless params[:home_address].blank?
+      @user                 = current_user
+      @user.avatar          = params[:avatar] unless params[:avatar].blank?
+      @user.avatar_id       = params[:avatar_id] unless params[:avatar_id].blank?
+      @user.qq              = params[:qq] unless params[:qq].blank?
+      @user.birthday        = params[:birthday] unless params[:birthday].blank?
+      @user.home_address    = params[:home_address] unless params[:home_address].blank?
       @user.school_name     = params[:school_name] unless params[:school_name].blank?
       @user.school_address  = params[:school_address] unless params[:school_address].blank?
       @user.parent_name     = params[:parent_name] unless params[:parent_name].blank?
