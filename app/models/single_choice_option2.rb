468b54042ab4b3bbf9ad1398c32aee8e3f1b91c4
dreamlx <dreamlinx@gmail.com>
2014-05-25 11:39:55 +0800

    fix

---
+++ app/models/single_choice_option2.rb
@@ -0,0 +1,17 @@
+class SingleChoiceOption2 < ActiveRecord::Base
+  belongs_to :question, class_name: 'Question::SingleChoice'
+
+  mount_uploader :image, ImageUploader
+  
+  acts_as_list scope: :question
+
+  attr_accessible :content, :correct, :position, :sequence, :_destroy, :image, :image_cache
+
+  validates :content, presence: true
+
+  default_value_for :correct, false
+
+  def alpha_position
+    ('A'.ord + self.position - 1).chr
+  end
+end
